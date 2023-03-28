0d4915454f4d60a4c71b9c49940355027af80b1c
Microwonk <nicolas.theo.frey@gmail.com>
2023-03-28 14:48:43 +0200

    randarray

---
+++ src/main/java/VALE/randomArray/RandomArray.java
@@ -0,0 +1,53 @@
+package VALE.randomArray;
+
+import java.util.*;
+import java.util.function.IntPredicate;
+
+public class RandomArray {
+    public static void main (String[] args) {
+        int[][] arr = randArray();
+        Arrays.stream(arr).forEach(candidate -> System.out.println(Arrays.toString(candidate)));
+        System.out.println();
+
+        sort2dArray(arr);
+        Arrays.stream(arr).forEach(candidate -> System.out.println(Arrays.toString(candidate)));
+    }
+
+    public static void sort2dArray(int[][] arr) {
+        final int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    for (int l = 0; l < n; l++) {
+                        if (arr[i][j] < arr[k][l]) {
+                            int temp = arr[i][j];
+                            arr[i][j] = arr[k][l];
+                            arr[k][l] = temp;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static int[][] randArray () {
+        Random r = new Random();
+        final int[][] res = new int[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                int rand = r.ints(1, 101)
+                        .filter(n -> Arrays.stream(res)
+                                .flatMapToInt(Arrays::stream)
+                                .noneMatch(equalsTo(n)))
+                        .findFirst()
+                        .orElseThrow();
+                res[i][j] = rand;
+            }
+        }
+        return res;
+    }
+
+    public static IntPredicate equalsTo(int target) {
+        return x -> x == target;
+    }
+}
